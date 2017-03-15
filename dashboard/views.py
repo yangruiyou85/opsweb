3ff36a953245cc08b0e6f0e3236e5e9b38910729
ruiyou.yang <ruiyou.yang@gmail.com>
2017-03-15 13:03:43 +0800

    change

---
+++ dashboard/views.py
@@ -0,0 +1,9 @@
+#-*- coding:utf-8 -*-
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>hello world.深圳你好.</h1>')
